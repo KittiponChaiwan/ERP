701b94dddf3aa85b581ba8f4b6a4fa93372ef1fe
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-08-10 09:00:41 +0700

    Change token to expire 1 hour

--- src/@core/utils/generateToken.js
+++ src/@core/utils/generateToken.js
@@ -7,7 +7,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY
 export const generateToken = payload => {
   // คีย์สำหรับเซ็น JWT (ส่วนตัว)
   // สร้าง JWT โดยใช้คีย์ส่วนตัว
-  const token = jwt.sign({ data: payload }, PRIVATE_KEY, { expiresIn: '3h' })
+  const token = jwt.sign({ data: payload }, PRIVATE_KEY, { expiresIn: '1h' })
 
-  Cookies.set('jwtToken', token, { expiresIn: '3h' })
+  Cookies.set('jwtToken', token)
 }
