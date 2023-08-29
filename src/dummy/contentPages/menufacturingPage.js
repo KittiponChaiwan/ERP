3de5722d6b5e9b94236fe39da6102ab090c14ec4
QuentinIce <100755139+QuentinIce@users.noreply.github.com>
2023-08-29 09:31:49 +0700

    stock-entry delete log amd Create work-order and menufacturing page

---
+++ src/dummy/contentPages/menufacturingPage.js
@@ -0,0 +1,24 @@
+const MenuWork_order = [
+  {
+    id: 1,
+    name: 'Production Item'
+  },
+  {
+    id: 2,
+    name: 'Configuration'
+  },
+  {
+    id: 3,
+    name: 'Operations'
+  },
+  {
+    id: 4,
+    name: 'More Info'
+  },
+  {
+    id: 5,
+    name: 'Connection'
+  }
+]
+
+export { MenuWork_order }
