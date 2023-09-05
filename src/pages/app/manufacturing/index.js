5fe77873a97ce54bff375f8fc95a2ba2d7a627c0
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-09-05 16:02:25 +0700

    Create Blank Page for navigation

---
+++ src/pages/app/manufacturing/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ManufacturingPage = () => {
+  return <div>ManufacturingPage</div>
+}
+
+export default ManufacturingPage
---
+++ src/pages/app/quality/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const QualityPage = () => {
+  return <div>QualityPage</div>
+}
+
+export default QualityPage
